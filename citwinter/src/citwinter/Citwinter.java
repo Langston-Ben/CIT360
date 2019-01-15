d1cd1f5786008df8ba92fc78286a79b0654164aa
benjaminlangston <benjaminlangston@10.0.0.85>
2019-01-15 00:07:14 -0600

    Completed 3 of 5 collection examples

--- citwinter/src/citwinter/Citwinter.java
+++ citwinter/src/citwinter/Citwinter.java
@@ -5,6 +5,8 @@
  */
 package citwinter;
 
+import java.util.*;
+
 /**
  *
  * @author benjaminlangston
@@ -16,6 +18,43 @@ public class Citwinter {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // ArrayList
+        List one = new ArrayList();
+        one.add("ObiWan");
+        one.add("Anakin");
+        one.add("Luke");
+        one.add("Yoda");
+        one.add("Vader");
+        
+        System.out.println("Jedi Knights");
+        System.out.println(one);
+        
+        // Set
+        int advance[] = {7,3,9,5,0,6,4};
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for (int i = 0; i < 7; i++) {
+            set.add(advance[i]);
+        }
+        System.out.println(set);
+        
+        //  Map
+        Map two = new HashMap();
+        two.put("Jimmy Page", "Guitar");
+        two.put("Robert Plant", "Vocals");
+        two.put("John Bonham", "Drums");
+        two.put("John Paul Jones", "Bass & Keyboards");
+        
+        System.out.println("Led Zeppelin");
+        System.out.println( two);
+        
+        // Tree
+        
+        // Queue
+        
+        
+
     }
     
 }
