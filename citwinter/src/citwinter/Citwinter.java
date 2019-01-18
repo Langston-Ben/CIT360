2c4f5f6ef3c9c1be72abc441d2d1e51ca2c5108b
benjaminlangston <benjaminlangston@10.0.0.85>
2019-01-17 23:11:06 -0600

    Five collections: Set, Tree, Map, List, Queue.

--- citwinter/src/citwinter/Citwinter.java
+++ citwinter/src/citwinter/Citwinter.java
@@ -26,32 +26,73 @@ public class Citwinter {
         one.add("Luke");
         one.add("Yoda");
         one.add("Vader");
-        
+        System.out.println("List Example");
         System.out.println("Jedi Knights");
-        System.out.println(one);
+        Iterator jedi = one.iterator();
+        
+        while(jedi.hasNext()) {
+        Object element = jedi.next();
+        System.out.print(element + "\n");
+        }
+        
+        System.out.print("\n");
         
         // Set
+        System.out.println("Set Example");
         int advance[] = {7,3,9,5,0,6,4};
-        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<>();
         
         for (int i = 0; i < 7; i++) {
             set.add(advance[i]);
         }
-        System.out.println(set);
+        System.out.println(set + "\n");
         
         //  Map
+        System.out.println("Map Example");
         Map two = new HashMap();
         two.put("Jimmy Page", "Guitar");
         two.put("Robert Plant", "Vocals");
         two.put("John Bonham", "Drums");
         two.put("John Paul Jones", "Bass & Keyboards");
         
-        System.out.println("Led Zeppelin");
-        System.out.println( two);
+        System.out.println("Led Zeppelin is: \n");
+        
+        two.forEach((k,v) -> System.out.println("Band Member - " + k + ", Instrument - " + v + "\n"));
+     
         
         // Tree
+        System.out.println("TreeSet Example");
+        TreeSet three = new TreeSet();
+        three.add("Black");
+        three.add("White");
+        three.add("Green");
+        three.add("Orange");
+        three.add("Yellow");
+        three.add("Red");
+        three.add("Purple");
+        
+        Iterator list = three.iterator();
+        
+        while(list.hasNext()) {
+        Object element = list.next();
+        System.out.print(element + "\n");
+        }
         
         // Queue
+        System.out.println("\nQueue Example");
+        Queue<String> pausedQueue = new LinkedList<>();
+        
+        pausedQueue.add("Jeff");
+        pausedQueue.add("Jim");
+        pausedQueue.add("Sam");
+        pausedQueue.add("Ben");
+        pausedQueue.add("Susan");
+        
+        pausedQueue.forEach(name -> {
+           System.out.println(name);
+        });
+        
+        
         
         
 
