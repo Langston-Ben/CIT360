e583f7c977c2a90f57866bb4a6655d3185a366a7
benjaminlangston <benjaminlangston@10.0.0.85>
2019-01-29 21:50:20 -0600

    Calculator example for Application Controller Pattern

--- AppContPatt/src/appcontpatt/AppContPatt.java
+++ AppContPatt/src/appcontpatt/AppContPatt.java
@@ -20,9 +20,8 @@ public class AppContPatt {
      */
     public static void main(String[] args) {
         
-        try { 
+     
             Scanner input = new Scanner(System.in);
-//            AppController myCalc = new AppController();
             Integer input1;
             Integer input2;
             
@@ -41,33 +40,6 @@ public class AppContPatt {
             System.out.println("The Answer is: ");
             AppController.applicationHandler(symbol, input1, input2);
             
-            
-        }catch (NumberFormatException ex) {
-            System.out.println(" ");
-            System.out.println("Please enter a correct number");
-            System.out.println(" ");
-            
-             Scanner input = new Scanner(System.in);
-//            AppController myCalc = new AppController();
-            Integer input1;
-            Integer input2;
-            
-            String symbol;
-            
-            System.out.println("Please enter a number");
-            input1 = Integer.parseInt(input.nextLine());
-            
-            System.out.println("Enter the mathmatical function you would like to do: \n"
-                    + "(+, -, *, /");
-            symbol = input.nextLine();
-            
-            System.out.println("Please enter another number");
-            input2 = Integer.parseInt(input.nextLine());
-            
-            System.out.println("The Answer is: ");
-            AppController.applicationHandler(symbol, input1, input2);
-        
-        }
     }
     
 }
