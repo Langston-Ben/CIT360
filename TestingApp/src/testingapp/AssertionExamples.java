bb30d04f7b76585251a5c0a72cdcb67bd34c15e6
benjaminlangston <benjaminlangston@10.0.0.85>
2019-01-31 21:55:10 -0600

    JUnit program first commit.

---
+++ TestingApp/src/testingapp/AssertionExamples.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package testingapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AssertionExamples {
+    
+    @Test
+    public void assertionExamples() {
+    
+    String one = "car";
+    String two = "car";
+    String three = null;
+    String four = "car";
+    String five = "car";
+    
+    int nOne = 7;
+    int nTwo = 3;
+    
+    String[] arrayOne = {"Peter", "James", "John"};
+    String[] arrayTwo = {"Peter", "James", "John"};
+    
+    System.out.println("assertEquals Test");
+    assertEquals(one,two);
+    
+    assertTrue(nTwo < nOne);
+    
+    assertFalse(nTwo > nOne);
+    
+    assertNotNull(one);
+    
+    assertNull(three);
+    
+    assertSame(four, five);
+    
+    assertNotSame(one, three);
+    
+    assertArrayEquals(arrayOne, arrayTwo);
+    
+}
+}
